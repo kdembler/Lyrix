c93844b3f3d7e0de2f2afc28a0c80572182c318f
Klaudiusz Dembler <k.dembler@gmail.com>
2018-01-28 20:48:19 +0100

    Add Genius search and browser window opening

--- SpotifyBar/AppDelegate.swift
+++ SpotifyBar/AppDelegate.swift
@@ -11,10 +11,13 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    var statusBarItem : NSStatusItem = NSStatusItem()
-    var menu : NSMenu = NSMenu()
-    var trackInfoMenuItem : NSMenuItem =  NSMenuItem()
-    var timer : Timer = Timer()
+    var statusBarItem: NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var trackInfoMenuItem: NSMenuItem =  NSMenuItem()
+    var timer: Timer = Timer()
+    var artist = ""
+    var track = ""
+    let bearerToken = "YOUR TOKEN HERE"
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         statusBarItem = NSStatusBar.system.statusItem(withLength: -1)
@@ -39,6 +42,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         timer.invalidate()
     }
     
+    func removeFeat(_ val: String) -> String {
+        var str = val
+        if let range = val.lowercased().range(of: " (feat.") {
+            str = String(val.prefix(upTo: range.lowerBound))
+        } else if let range = val.lowercased().range(of: " feat.") {
+            str = String(val.prefix(upTo: range.lowerBound))
+        }
+        return str
+    }
+    
+    @objc func getLyrics() {
+        let escaped = "\(artist) \(track)".addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+        let apiPrefix = "https://api.genius.com/search?q=\(escaped)"
+        guard let searchURL = URL(string: apiPrefix) else { return }
+        var request = URLRequest(url: searchURL)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in
+            if error == nil, let result = data {
+                guard let json = try? JSONSerialization.jsonObject(with: result, options: []) else { return }
+                if let dict = json as? [String: Any] {
+                    if let resp = dict["response"] as? [String: Any] {
+                        if let hits = resp["hits"] as? [Any] {
+                            if hits.count > 0 {
+                                if let hit = hits[0] as? [String: Any] {
+                                    if let info = hit["result"] as? [String: Any] {
+                                        if let songPath = info["api_path"] as? String {
+                                            if let songURL = URL(string: "https://genius.com" + songPath) {
+                                                // I really hope there is some way to do this with less nesting
+                                                NSWorkspace.shared.open(songURL)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
     @objc func quit() {
         NSApplication.shared.terminate(self)
     }
@@ -47,12 +94,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // TODO: make fetch async
         var v = ""
         do {
-            let (artist, track) = try SpotifyApi.getArtistAndTitle()
+            (artist, track) = try SpotifyApi.getArtistAndTitle()
+            artist = removeFeat(artist)
+            track = removeFeat(track)
             v = "\(track) by \(artist)"
-            // TODO: figure out a way for finding genius lyrics
+            trackInfoMenuItem.action = #selector(getLyrics)
         } catch {
             v = "Fetch error"
+            trackInfoMenuItem.action = nil
         }
+        Swift.print(v)
         trackInfoMenuItem.title = v
     }
 }
@@ -69,7 +120,7 @@ struct SpotifyApi {
         return (artist, title)
     }
     
-    static func executeScript(_ query: String) throws -> String {
+    static private func executeScript(_ query: String) throws -> String {
         let script = NSAppleScript(source: "\(prefix) \(query)")
         var err : NSDictionary?
         let result = script?.executeAndReturnError(&err)
